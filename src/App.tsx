191267dfe4dcc666ef328ccb5b4b2b62188f6c97
viperofficailll <sharadhapokharel777@gmal.com>
2026-02-03 21:23:49 +0545

    feat(Hero): First hero section with gradient effects and inset line

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import Approutes from "./Routes/Approutes";
+
+function App() {
+  return (
+    <>
+      <div className="flex h-screen justify-center bg-neutral-900">
+        <Approutes></Approutes>
+      </div>
+    </>
+  );
+}
+
+export default App;
